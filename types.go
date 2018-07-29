4f971b92728406f281109c625726232a7a2101aa
Gennady Trafimenkov <gennady.trafimenkov@gmail.com>
2018-07-29 18:37:47 +0300

    remove all mentions of github.com/cenkalti/backoff library

--- types.go
+++ types.go
@@ -5,9 +5,6 @@ package hetzner
 import (
 	"fmt"
 	"net/http"
-	"time"
-
-	"github.com/cenkalti/backoff"
 )
 
 // An ErrorResponse reports the error caused by an API request
@@ -22,32 +19,4 @@ type ErrorResponse struct {
 func (r *ErrorResponse) Error() string {
 	return fmt.Sprintf("%v %v: %d %v",
 		r.Response.Request.Method, r.Response.Request.URL, r.Response.StatusCode, r.Message)
-}
-
-// Default values for ExponentialBackOff.
-const (
-	DefaultInitialInterval     = 50 * time.Millisecond
-	DefaultRandomizationFactor = 0.5
-	DefaultMultiplier          = 1.5
-	DefaultMaxInterval         = 5 * time.Second
-	DefaultMaxElapsedTime      = 60 * time.Second
-)
-
-// NewExponentialBackOff creates an instance of ExponentialBackOff using default values.
-func NewExponentialBackOff() *backoff.ExponentialBackOff {
-	b := &backoff.ExponentialBackOff{
-		InitialInterval:     DefaultInitialInterval,
-		RandomizationFactor: DefaultRandomizationFactor,
-		Multiplier:          DefaultMultiplier,
-		MaxInterval:         DefaultMaxInterval,
-		MaxElapsedTime:      DefaultMaxElapsedTime,
-		Clock:               backoff.SystemClock,
-	}
-	if b.RandomizationFactor < 0 {
-		b.RandomizationFactor = 0
-	} else if b.RandomizationFactor > 1 {
-		b.RandomizationFactor = 1
-	}
-	b.Reset()
-	return b
 }
